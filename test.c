ebfedba56b6326636662d06e91ccee3d84836f0f
Satadru Mallick <satadru.cs@gmail.com>
2018-06-17 23:18:16 +0530

    Create test.c

---
+++ test.c
@@ -0,0 +1,19 @@
+/*
+    https://www.codechef.com/problems/TEST
+    https://www.codechef.com/viewsolution/14190491
+*/
+
+#include<stdio.h>
+int main() 
+{
+	int n;
+	while(1)
+	{
+		scanf("%d",&n);
+		if(n!=42)
+			printf("%d\n",n);
+		else
+			break;
+	}
+	return 0;
+} 
